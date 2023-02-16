e62086e1e5413e7cee92721a91bea89691b8982d
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-02-16 11:39:22 +0200

    modified Person class to inherit from Nameable

--- person.rb
+++ person.rb
@@ -1,26 +1,30 @@
-# Person class
-class Person
-  attr_reader :id, :age
-  attr_accessor :name
+# Define the base Decorator class that inherits from Nameable
+require_relative 'nameable'
+class Person < Nameable
+  attr_accessor :id, :name, :age, :parent_permission
 
-  def initialize(age, parent_permission: true, name: 'Unknown')
+  def initialize(age, parent_permission: true, name: "Unknown")
     @id = generate_id
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def can_use_service?
-    is_of_age || @parent_permission
+  def can_use_services?
+    is_of_age? || @parent_permission
   end
 
-  def of_age?
+  def is_of_age?
     @age >= 18
+  end
+
+  def correct_name
+    name
   end
 
   private
 
   def generate_id
-    Random.rand(1..1000)
+    rand(1..1000)
   end
 end
