df58eac3a46f4205b88f67ae991db44822adf8b9
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-02-16 11:38:14 +0200

    add Decorator class that inherits from Nameable

---
+++ base_decorator.rb
@@ -0,0 +1,11 @@
+# Define the base Decorator class that inherits from Nameable
+require_relative 'nameable'
+class Decorator < Nameable
+    def initialize(nameable)
+        @nameable = nameable
+    end
+
+    def correct_name
+        @nameable.correct_name
+    end
+end
