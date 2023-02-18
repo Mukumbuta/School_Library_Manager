bf4bdf66c810781fb30b999b07cc6813c47a0b1d
Mukumbuta <mukumbutasimasiku@gmail.com>
2023-02-18 23:12:21 +0200

    fix linter errors

--- students.rb
+++ students.rb
@@ -2,13 +2,17 @@
 require_relative 'person'
 
 class Student < Person
-  attr_reader :name, :rentals
-  attr_accessor :classroom
+  attr_reader :name, :rentals, :classroom
 
   def initialize(name)
     super()
     @name = name
     @rentals = []
     @classroom = nil
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 end
